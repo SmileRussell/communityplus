20ef5a03627b50d014a32f77d97d5fdce0bce444
SmileRussell <921746098@qq.com>
2021-07-01 23:05:54 +0800

    Community

---
+++ src/main/java/com/dingweichang/communityplus/CommunityplusApplication.java
@@ -0,0 +1,13 @@
+package com.dingweichang.communityplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingweichang/communityplus/CommunityplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingweichang.communityplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
